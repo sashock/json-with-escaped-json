83cbe4380eb6c1613e8e109e6b21a7ac4e2a083e
sashock <sashock@gmail.com>
2021-05-27 16:18:55 +0200

    added hardcoded keys for json deserialization

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,27 +1,30 @@
-import { NullTemplateVisitor } from "@angular/compiler";
-import { Component } from "@angular/core";
-import _ from "lodash";
+import { NullTemplateVisitor } from '@angular/compiler';
+import { Component } from '@angular/core';
+import _ from 'lodash';
 
 const mapKeysDeep = (obj, cb) =>
   _.mapValues(_.mapKeys(obj, cb), val =>
     _.isObject(val) ? mapKeysDeep(val, cb) : val
   );
 
-const mapValuesDeep = (obj, fn) =>
+const mapValuesDeep = (obj, fn, index:string=null) =>
   _.mapValues(obj, (val, key) => {
     if (_.isPlainObject(val)) {
       mapValuesDeep(val, fn);
     } else if (_.isArray(val)) {
-      val.forEach(subval => mapValuesDeep(subval, fn));
+      for (var arrayKey in val) {
+      val.forEach(subval => mapValuesDeep(subval, fn, arrayKey));
+
+}
     } else {
       fn(val, key, obj);
     }
   });
 
 @Component({
-  selector: "my-app",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   myObj;
@@ -30,18 +33,23 @@ export class AppComponent {
     let parsed = null;
     try {
       parsed = JSON.parse(this.jsonInput);
-      mapValuesDeep(parsed, (key, val, obj) => {
-        if (obj["key"] && obj["value"] && obj["value"].length > 3) {
-          try {
-            const nestedParsed = JSON.parse(obj["value"]);
+      this.jsonInput = '';
 
-            obj["value"] = nestedParsed;
+        if (
+          obj['key'] == 'custom_view_configuration' ||
+          obj['key'] == 'custom_dashboard_configuration' ||
+          obj['key'] == 'view-hierarchy-configuration'
+        ) {
+          try {
+            const nestedParsed = JSON.parse(obj['value']);
+            obj['value'] = nestedParsed;
+            console.log(key);
           } finally {
           }
         }
       });
     } catch (e) {
-      console.error("invalid json");
+      console.error('invalid json');
     } finally {
       if (parsed) {
         console.log(parsed);
